bf0cf23543882f04e0bcf5833128f19a34c65408
Pamela BouKhalil <pamela.boukhalil@net.usj.edu.lb>
2023-10-09 20:11:20 +0300

    added database_base_connection_withlogging_handler in order to integrate the logging handler customized for the project + modified some line in loggin_handler+ config2: in order to configure the connection to my database

--- logging_handler.py
+++ logging_handler.py
@@ -1,15 +1,28 @@
 import logging
 
 class Logger:
-    def __init__(self, log_level=logging.DEBUG):
+  def __init__(self, log_file=None):
+        self.log_file = log_file  # Store the log file name as an instance variable
+        self._log_level = logging.DEBUG  # Default log level is DEBUG
+        self._configure_logger()
+
+  def _configure_logger(self):
+        mode = 'w' if self.log_file else 'a'  # Use 'w' (write mode) if a log file is specified
         logging.basicConfig(
             format="%(asctime)s;%(levelname)s;%(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
-            level=log_level
+            level=self._log_level,
+            handlers=[
+                logging.FileHandler(self.log_file, mode=mode) if self.log_file else logging.StreamHandler()
+            ]
         )
         self.logger = logging.getLogger()
 
-    def log_message(self, log_mess):
+  def set_log_level(self, log_level):
+        self._log_level = log_level
+        self.logger.setLevel(log_level)
+
+  def log_message(self, log_mess):
         if self.logger.level == logging.DEBUG:
             self.logger.debug(log_mess)
         elif self.logger.level == logging.INFO:
