7330df17a23626582622d1207b93e9e2b8b0d9d7
maria.hourani <maria.hourani@net.usj.edu.lb>
2023-10-30 23:48:09 +0200

    Staging, Dimension and facts table added

--- pandas_handler.py
+++ pandas_handler.py
@@ -2,21 +2,25 @@ import pandas as pd
 import re
 
 # Function to remove non-UTF-8 characters from text
+
+
 def clean_text(text):
     non_utf8_pattern = re.compile('[^\x00-\x7F]+')
     cleaned_text = re.sub(non_utf8_pattern, '', text)
     return cleaned_text
 
 # Function to clean all text columns in a DataFrame
+
+
 def clean_data_from_utf8(df):
     # Apply the clean_text function to all text columns
     for col in df.select_dtypes(include=['object']).columns:
         df[col] = df[col].apply(clean_text)
     return df
 
+
 def load_data_csv(file_path):
     return pd.read_csv(file_path)
-
 
 
 def explore_data(data):
@@ -35,6 +39,5 @@ def preprocess_data(data):
 # def main():
 #     file_path = 'Spotify_data(1).xlsx'
 #     music_data = load_data(file_path)
-    
+
 #     explore_data(music_data)
-    
