04df7ffc6c3284f6d337b8fab9e780a35b2bae7a
Pamela BouKhalil <pamela.boukhalil@net.usj.edu.lb>
2023-11-11 12:47:00 +0200

    commit

--- prehook.py
+++ prehook.py
@@ -1,20 +1,17 @@
 import data_handler
 import lookups
 import db_handler
-<<<<<<< HEAD
+
 from data_handler import return_create_statement_from_dataframe
 import pandas as pd
 from db_handler import create_connection,execute_query, close_connection
 from data_handler import insert_statements_from_dataframe,extract_data_into_df,create_etl_watermark_table,insert_data_in_batches
 from lookups import FileType
 
-db_session = create_connection()
-=======
 import os
 import googleapiclient
 import google.oauth2
 
->>>>>>> pam
 
 # try to load them in google drive and use a common link between the team.
 def return_csv_list():
@@ -70,37 +67,7 @@ def execute_prehook_sql_statements(db_session):
         db_handler.execute_query(db_session, file_content)
 
 
-def create_staging_table():
-    stg_users_dataframe = pd.DataFrame({
-        "age": [25, 30, 35],
-        "gender": ['Male', 'Female', 'Male'],
-        "fav_music_genre": ['Pop', 'Rock', 'Hip-Hop'],
-        "music_time_slot": ['Morning', 'Evening', 'Afternoon'],
-        "music_lis_frequency": ['Daily', 'Weekly', 'Monthly'],
-        "music_satisfaction": ['High', 'Medium', 'Low']
-    })
-
-    stg_songs_dataframe = pd.DataFrame({
-        "track_id": [1, 2, 3],
-        "track_name": ["Song1", "Song2", "Song3"],
-        "track_artist": ["Artist1", "Artist2", "Artist3"],
-        "track_popularity": [80, 90, 70],
-        "track_album_id": [101, 102, 103],
-        "track_album_name": ["Album1", "Album2", "Album3"],
-        "track_album_release_date": ["2022-01-01", "2022-02-01", "2022-03-01"],
-        "playlist_name": ["Playlist1", "Playlist2", "Playlist3"],
-        "playlist_id": [201, 202, 203],
-        "playlist_genre": ["Genre1", "Genre2", "Genre3"],
-        "energy": [0.8, 0.7, 0.6],
-        "key": [1, 2, 3],
-        "loudness": [-5.0, -6.0, -7.0],
-        "mode": [0, 1, 0],
-        "speechiness": [0.1, 0.2, 0.3],
-        "valence": [0.7, 0.6, 0.5],
-        "tempo": [120, 130, 140],
-        "duration_ms": [240000, 250000, 260000]
-    })
-
+    
 
 def create_staging_tables(csv_list, staging_source_name, staging_schema_name, staging_table_name):
     # recommendation: using an underscore statement like: music_schema.
