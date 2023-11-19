6ac2cdfec4b7a81f644b9d606544f14c31e8d8e0
Pamela BouKhalil <pamela.boukhalil@net.usj.edu.lb>
2023-11-19 12:38:01 +0200

    added prehook data_handler and adjusted etlwatermark table

--- SQL_Commands/V4_prehook_Create_ETL_watermark_table.sql
+++ SQL_Commands/V4_prehook_Create_ETL_watermark_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS musicschema.etl_watermark
 (
   ID SERIAL PRIMARY KEY NOT NULL,
-  table_name VARCHAR(50) PRIMARY KEY,
+  table_name VARCHAR(50),
   last_update_timestamp TIMESTAMP
 );
